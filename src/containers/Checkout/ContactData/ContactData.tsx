62cd4a27fd2b81bb9ee1d5bb6624730af487934d
Raymel Manreza <raymel.manreza@gmail.com>
2020-06-19 14:18:18 -0400

    ContactData work in progress

--- src/containers/Checkout/ContactData/ContactData.tsx
+++ src/containers/Checkout/ContactData/ContactData.tsx
@@ -3,6 +3,96 @@ import classes from "./ContactData.module.css";
 import Button from "../../../components/UI/Button/Button";
 
 class ContactData extends Component {
+  state = {
+    orderForm: {
+      name: {
+        elementType: "input",
+        elementConfig: {
+          type: "text",
+          placeholder: "Your Name",
+        },
+        value: "",
+        validation: {
+          required: true,
+        },
+        valid: false,
+        touched: false,
+      },
+      street: {
+        elementType: "input",
+        elementConfig: {
+          type: "text",
+          placeholder: "Street",
+        },
+        value: "",
+        validation: {
+          required: true,
+        },
+        valid: false,
+        touched: false,
+      },
+      zipCode: {
+        elementType: "input",
+        elementConfig: {
+          type: "text",
+          placeholder: "ZIP Code",
+        },
+        value: "",
+        validation: {
+          required: true,
+          minLength: 5,
+          maxLength: 5,
+        },
+        valid: false,
+        touched: false,
+      },
+      country: {
+        elementType: "input",
+        elementConfig: {
+          type: "text",
+          placeholder: "Country",
+        },
+        value: "",
+        validation: {
+          required: true,
+        },
+        valid: false,
+        touched: false,
+      },
+      email: {
+        elementType: "input",
+        elementConfig: {
+          type: "email",
+          placeholder: "Your E-Mail",
+        },
+        value: "",
+        validation: {
+          required: true,
+        },
+        valid: false,
+        touched: false,
+      },
+      deliveryMethod: {
+        elementType: "select",
+        elementConfig: {
+          options: [
+            { value: "fastest", displayValue: "Fastest" },
+            { value: "cheapest", displayValue: "Cheapest" },
+          ],
+        },
+        value: "fastest",
+        validation: {},
+        valid: true,
+      },
+    },
+    formIsValid: false,
+    archive: false, //set archive property only when form is submitted
+  };
+
+  orderHandler = (event: any) => {
+    event.preventDefault();
+  };
+
   render() {
     return (
       <div className={classes.ContactData}>
@@ -11,7 +101,9 @@ class ContactData extends Component {
         <input type="email" name="email" placeholder="Your email" />
         <input type="text" name="street" placeholder="Street" />
         <input type="text" name="postal" placeholder="Your zip code" />
-        <button>ORDER</button>
+        <Button btnType="Success" clicked={this.orderHandler}>
+          ORDER
+        </Button>
       </div>
     );
   }
